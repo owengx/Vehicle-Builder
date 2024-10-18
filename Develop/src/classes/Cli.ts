04d58cccf32edfcd3b64d2fccea8d5ce59736c0c
owengx <owengayle19@gmail.com>
2024-10-17 22:06:54 -0400

    findvehicle to tow method implemented, perform wheelie else if line 406. previously not in code. could not get to work prior. used similarly to findvehicle to tow method

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -392,6 +392,28 @@ class Cli {
         }
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
+        else if (answers.action === 'Tow a vehicle') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Truck) {
+                this.findVehicleToTow(this.vehicles[i] as Truck); return
+              } else {
+                console.log(`This vehicle cant tow.`);
+              }
+            }
+          }
+        }
+        else if (answers.action === 'Perform a wheelie') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Motorbike) {
+                (this.vehicles[i] as Motorbike).wheelie();
+              } else {
+                console.log(`This vehicle cant wheelie.`);
+              }
+            }
+          }
+        }
         else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
